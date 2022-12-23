892a9c73e217ae140a2a53ae2476913e2357c32b
Ali Can <alibugraoglu@gmail.com>
2022-12-23 23:09:50 +0300

    day41_ExceptionsIntro

--- src/day41_ExceptionsIntro/Check_vs_Unchecked.java
+++ src/day41_ExceptionsIntro/Check_vs_Unchecked.java
@@ -1,9 +1,14 @@
 package day41_ExceptionsIntro;
 
+import day39_Recap.cydeoTask.Student;
+
+import java.io.FileInputStream;
+import java.util.Locale;
+
 public class Check_vs_Unchecked {
 
     public static void main(String[] args) {
-        // unchecked Exception;
+        // unchecked Exception; occurs during the runtime
 
         int a = 10;
         int b = 0;
@@ -14,6 +19,42 @@ public class Check_vs_Unchecked {
         char[] characters = {'A','B','C'};
         //                    0    1  2
         //System.out.println(characters[88]); //all the exceptions in java from Java.lang. package;
+
+        Student student = null;// this null keyword does not reference any object;object doesn't exist means u cannot use the instance;
+
+        //System.out.println(student.getName());//unchecked exception;
+
+        //student.study();
+
+        String str = "Silver Spoon"; // now this object becomes unreferenced and all unreferenced objects are eligible for garbage collection;
+        //but by using final keyword we can prevent the object to be collected by garbage collector;
+        //str= null;// null is not object
+
+       // System.out.println(str.toUpperCase());
+
+        String str1 = ""; // even this is empty string but the object != null; even this string is blank  it is also string object;
+        System.out.println(str1.isEmpty());
+
+        //checked exception; occurs during the compile time
+
+        System.out.println("Hello");
+
+        //Thread.sleep(2000);// needs immediate handle;
+
+        System.out.println("Cydeo");
+
+        //FileInputStream file = new FileInputStream("path of the file");// but here compile is not happy easy to tell checked exception;
+
+        System.out.println("Java".charAt(33));// unchecked exception compile is ok with that.
+
+
+
+
+
+
+
+
+
 
 
 
--- src/day41_ExceptionsIntro/TryCatchBlocks.java
+++ src/day41_ExceptionsIntro/TryCatchBlocks.java
@@ -21,22 +21,37 @@ public class TryCatchBlocks {
         System.out.println("-------------------------");
 
         System.out.println("Test2 started");
-        int[] numbers = {1,2,3,4,5};
+        int[] numbers = {1, 2, 3, 4, 5};
 
-try {
-    System.out.println(numbers[200]);
-    System.out.println("Try Block");
-}catch(ArrayIndexOutOfBoundsException e1){
-    System.out.println("Catch block");
-    System.out.println("Index Out of Bounds Exception occurred.");
+        try {
+            System.out.println(numbers[200]);
+            System.out.println("Try Block");
+        } //catch (ArrayIndexOutOfBoundsException e1) {// you cannot use any other Exceptions class here;if you don't know which exception class without executing the code you had better use
+          // parent Exception class which is Runtime Exception class;
+        catch (RuntimeException a) {
+            a.printStackTrace();// most preferred one if the exception happens during the runtime;gives full details of the exception;
+            //System.out.println(a.getMessage());//.getMessage(); gives oyu brief description of exceptions;
 
-}
 
+            /*System.out.println("Catch block");
+            //System.out.println("Index Out of Bounds Exception occurred.");
+            System.out.println("Runtime Exception was occurred.");
 
+             */
+        }
         System.out.println("Test2 completed.");
 
 
+        System.out.println("------------------------------");
 
+        System.out.println("Test3 started.");
+        try{
+        System.out.println("Cydeo".substring(2,0)); // might give you uncheck exception(runtime exception) and compile cannot tell what the exception type is.
+
+        }catch (RuntimeException b){
+            b.printStackTrace();
+        }
+        System.out.println("Test3 completed.");
 
 
     }
