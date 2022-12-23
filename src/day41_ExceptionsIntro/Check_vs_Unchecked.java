fd46f9bb6d4741cdaf52e555afb046ace1fcecda
Ali Can <alibugraoglu@gmail.com>
2022-12-23 15:18:42 +0300

    day41_ExceptionsIntro

---
+++ src/day41_ExceptionsIntro/Check_vs_Unchecked.java
@@ -0,0 +1,26 @@
+package day41_ExceptionsIntro;
+
+public class Check_vs_Unchecked {
+
+    public static void main(String[] args) {
+        // unchecked Exception;
+
+        int a = 10;
+        int b = 0;
+
+        //System.out.println(a / b);// unchecked exception at the same time it would terminate your code;
+       // System.out.println("Silver Spoon");// cannot executed;
+
+        char[] characters = {'A','B','C'};
+        //                    0    1  2
+        //System.out.println(characters[88]); //all the exceptions in java from Java.lang. package;
+
+
+
+
+
+
+    }
+
+
+}
