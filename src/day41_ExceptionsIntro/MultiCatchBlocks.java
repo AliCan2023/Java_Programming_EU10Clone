7ca43b89e207c522ce625e245c483caa353222e3
Ali Can <alibugraoglu@gmail.com>
2022-12-25 15:44:16 +0300

    day41_Exceptions_Intro

--- src/day41_ExceptionsIntro/MultiCatchBlocks.java
+++ src/day41_ExceptionsIntro/MultiCatchBlocks.java
@@ -1,6 +1,7 @@
 package day41_ExceptionsIntro;
 
 
+import com.sun.security.jgss.GSSUtil;
 import day39_Recap.cydeoTask.Employee;
 
 public class MultiCatchBlocks {
@@ -12,18 +13,30 @@ public class MultiCatchBlocks {
         try {
 
             System.out.println(employee.getSalary());// what will be the salary of employee???//NullPointerException
+
+        } catch (NullPointerException e) {
+            System.out.println("First catch block.");
         } catch (IndexOutOfBoundsException e) {
-            System.out.println(" First catch block");
+            System.out.println(" Second catch block");
             e.printStackTrace();
         } catch (ArithmeticException e) {
-            System.out.println("Second catch block");
+            System.out.println("Third catch block");
             e.printStackTrace();
         } catch (ClassCastException e) {
-            System.out.println("Third Catch block");// this catch block catches the fish ;
+            System.out.println("Fourth Catch block");// this catch block catches the fish ;
             e.printStackTrace();
-        }catch (RuntimeException e){
-            System.out.println("Fourth catch block.");
+        } catch (RuntimeException e) {
+            System.out.println("Fifth catch block.");
             e.printStackTrace();
         }
+        System.out.println("Test Completed");
+
+        System.out.println("------------------------------------------------------");
+        try{
+
+        System.out.println("Java".charAt(-1));
+    }catch (RuntimeException e ){
+            e.printStackTrace();
+        }
+        }
     }
-}
