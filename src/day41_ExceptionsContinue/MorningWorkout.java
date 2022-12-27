fde7ef620c5747a5e2f60c5da045a50a3d5ca23d
Ali Can <alibugraoglu@gmail.com>
2022-12-27 23:32:57 +0300

    day41_ExceptionsContinue

---
+++ src/day41_ExceptionsContinue/MorningWorkout.java
@@ -0,0 +1,21 @@
+package day41_ExceptionsContinue;
+
+public class MorningWorkout {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+}
+/*
+Warmup task:
+	MorningWorkOut:
+			1. Do 30 push-ups and try pausing 1.5 seconds in each
+
+
+			2. Do 20 Pull-ups and try pausing 2.5 seconds in each
+ */
